47b0fd8b0690b980c212b9a0b8a62021eb6c5172
carjes80 <salcedoc.fn@gmail.com>
2022-09-20 16:37:50 -0300

    Inicio de JavaScript

---
+++ Resumen/js/script.js
@@ -0,0 +1,4 @@
+var usuario = prompt("Comisión")
+if (usuario == "22516") {
+    document.write("<div class='centrado'><iframe width='560' height='315' src='https://www.youtube.com/embed/EQg2NYQ5usg' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe></div>")
+}
