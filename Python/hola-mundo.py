3084b4597a029e8d12494c196a98f57c0b68a596
carjes80 <salcedoc.fn@gmail.com>
2022-11-01 18:34:51 -0300

    Clase introductoria de Phyton

---
+++ Python/hola-mundo.py
@@ -0,0 +1,37 @@
+# print("Hola Mundo") #Linea de comentario
+# Los comentarios en bloque se hacen con triple comilla simple o doble
+""" Se usa las comillas dobles en el programa para explicar el código.
+Todo esto es un comentario
+"""
+''' Se usan para DOCSTRINGS (Se usan para generar la documentación del programa)
+Esto tambien es comentario
+'''
+
+# Tipos de Datos
+texto = "Estamos aprendiendo Python"  # String
+print(texto)
+print(type(texto))
+
+entero = 4
+print(entero)
+print(type(entero))
+
+decimal = 4.21
+print(decimal)
+print(type(decimal))
+
+decimal2 = 4.21
+print(decimal2 + 5)
+print(type(decimal2 + 5))
+
+e = 20
+print(3*(200-e))
+print(3*200-e)
+
+# Sentencias divididas en lineas
+a = 2+5+4+1+2\
+    + 5+4+2 +\
+    5
+print(3)
+print(a)
+#Indentación es la manera en la que se organiza el código
